0498e8dcde5a7e9ec28ca07f4b2c1235430e44e0
lcinrain <lcinrain@sina.com>
2024-03-25 22:45:34 +0800

    cfg file

---
+++ cfg.py
@@ -0,0 +1,7 @@
+cfg_tg = {'database':'ipv6-tg','user':'root','password':'root'}
+
+HEX_STR = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+server_url = 'http://127.0.0.1:8001'
+srv_list = ['127.0.0.1','127.0.0.1']
+port = 8001
+URLs = [f'http://{ip}:{port}' for ip in srv_list]
